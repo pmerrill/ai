e68f40552e8be66a1413c3b69c00ff4e439145e7
Peter Merrill <petejmerrill@gmail.com>
2020-02-06 15:03:50 -0600

    Setting up

--- story/views.py
+++ story/views.py
@@ -19,6 +19,37 @@ from GPT2.encoder import get_encoder
 # Create your views here.
 def index(request):
 
+    def text_generator(state_dict, param_prompt, param_nsamples, param_batch_size, param_length, param_temperature, param_top_k):
+
+        #param_prompt = "Peter was a man"
+        param_quiet = False
+        #param_nsamples = 1
+        param_unconditional = None
+        #param_batch_size = 1
+        #param_length = 5
+        #param_temperature = 0.95
+        #param_top_k = 100
+
+        if param_batch_size == -1:
+            param_batch_size = 1
+        assert param_nsamples % param_batch_size == 0
+
+        seed = random.randint(0, 2147483647)
+        np.random.seed(seed)
+        torch.random.manual_seed(seed)
+        torch.cuda.manual_seed(seed)
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+        # Load Model
+        enc = get_encoder()
+        config = GPT2Config()
+        model = GPT2LMHeadModel(config)
+        model = load_weight(model, state_dict)
+        model.to(device)
+        model.eval()
+
+        return param_prompt + str('... worked!')
+
     message = 'Error'
 
     if os.path.exists('gpt2-pytorch_model.bin'):
@@ -33,8 +64,8 @@ def index(request):
             param_top_k = int(request.GET.get('i', 50000))
 
             try:
-                message = param_prompt, param_nsamples, param_batch_size, param_length, param_temperature, param_top_k
-                #message = text_generator(state_dict, param_prompt, param_nsamples, param_batch_size, param_length, param_temperature, param_top_k)
+                #message = param_prompt, param_nsamples, param_batch_size, param_length, param_temperature, param_top_k
+                message = text_generator(state_dict, param_prompt, param_nsamples, param_batch_size, param_length, param_temperature, param_top_k)
             except Exception as ex:
                 message = "Please enter a prompt in the form below."
 
